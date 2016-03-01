a95497aac6ad31578dca94b8fcf34443c827789c
Vibhaj Rajan <vibhaj8@gmail.com>
2016-03-01 18:40:47 +0530

    getheader python 3

--- browserstack/local_binary.py
+++ browserstack/local_binary.py
@@ -47,7 +47,10 @@ class LocalBinary:
 
   def download(self, chunk_size=8192, progress_hook=None):
     response = urlopen(self.http_path)
-    total_size = int(response.info().get_all('Content-Length').strip())
+    try:
+      total_size = int(response.info().getheader('Content-Length').strip())
+    except:
+      total_size = int(response.info().get_all('Content-Length')[0].strip())
     bytes_so_far = 0
 
     dest_parent_dir = self.__available_dir()
