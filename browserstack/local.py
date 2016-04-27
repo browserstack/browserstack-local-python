e7b1aa9edab16689b69595fc891778850a8816e1
Vibhaj Rajan <vibhaj8@gmail.com>
2016-04-28 01:49:16 +0530

    added dynamic args

--- browserstack/local.py
+++ browserstack/local.py
@@ -3,37 +3,42 @@ from browserstack.local_binary import LocalBinary
 from browserstack.bserrors import BrowserStackLocalError
 
 class Local:
-  def __init__(self, key=None, binary_path=None):
-    self.options = {
-      'key': key,
-      'logfile_flag': '-logFile',
-      'logfile_path': os.path.join(os.getcwd(), 'local.log')
-    }
-    self.local_folder_path = None
-    self.local_logfile_path = self.options['logfile_path']
+  def __init__(self, key=os.environ['BROWSERSTACK_ACCESS_KEY'], binary_path=None):
+    self.key = key
+    self.options = None
+    self.local_logfile_path = os.path.join(os.getcwd(), 'local.log')
 
-  def __xstr(self, obj):
-    if obj is None:
-      return ''
-    return str(obj)
+  def __xstr(self, key, value):
+    if key is None:
+      return ['']
+    if str(value).lower() == "true":
+      return ['-' + key]
+    else:
+      return ['-' + key, value]
 
   def _generate_cmd(self):
-    options_order = ['logfile_flag', 'logfile_path', 'folder_flag', 'key', 'folder_path', 'forcelocal', 'local_identifier', 'only', 'only_automate', 'proxy_host', 'proxy_port', 'proxy_user', 'proxy_pass', 'forceproxy', 'force', 'verbose', 'hosts']
-    cmd = [self.__xstr(self.options.get(o)) for o in options_order if self.options.get(o) is not None]
-    return [self.binary_path] + cmd
+    cmd = [self.binary_path, '-logFile', self.local_logfile_path, self.key]
+    for o in self.options.keys():
+      if self.options.get(o) is not None:
+        cmd = cmd + self.__xstr(o, self.options.get(o))
+    return cmd
 
   def start(self, **kwargs):
-    for key, value in kwargs.items():
-      self.__add_arg(key, value)
-    
+    self.options = kwargs
+
     if 'binarypath' in self.options:
       self.binary_path = binary_path
     else:
       self.binary_path = LocalBinary().get_binary()
 
+    if 'logfile' in self.options:
+      self.local_logfile_path = self.options['logfile']
+      del self.options['logfile']
+
     if "onlyCommand" in kwargs and kwargs["onlyCommand"]: 
       return
 
+    print self._generate_cmd()
     self.proc = subprocess.Popen(self._generate_cmd(), stdout=subprocess.PIPE)
     self.stderr = self.proc.stderr
 
@@ -55,43 +60,6 @@ class Local:
     if (hasattr(self, 'proc')):
       return True if self.proc.poll() is None else False
     return False
-
-  def __add_arg(self, key, value):
-    if key == 'verbose' and value:
-      self.options['verbose'] = '-v'
-    elif key == 'force' and value:
-      self.options['force'] = '-force'
-    elif key == 'only' and value:
-      self.options['only'] = '-only'
-    elif key == 'onlyAutomate' and value:
-      self.options['only_automate'] = '-onlyAutomate'
-    elif key == 'forcelocal' and value:
-      self.options['forcelocal'] = '-forcelocal'
-    elif key == 'localIdentifier':
-      self.options['local_identifier'] = '-localIdentifier ' + str(value)
-    elif key == 'f':
-      self.options['folder_flag'] = '-f'
-      self.options['folder_path'] = str(value)
-    elif key == 'proxyHost':
-      self.options['proxy_host'] = '-proxyHost ' + str(value)
-    elif key == 'proxyPort':
-      self.options['proxy_port'] = '-proxyPort ' + str(value)
-    elif key == 'proxyUser':
-      self.options['proxy_user'] = '-proxyUser ' + str(value)
-    elif key == 'proxyPass':
-      self.options['proxy_pass'] = '-proxyPass ' + str(value)
-    elif key == 'hosts':
-      self.options['hosts'] = str(value)
-    elif key == 'forceproxy' and value:
-      self.options['forceproxy'] = '-forceproxy'
-    elif key == 'logfile':
-      self.options['logfile_flag'] = '-logFile'
-      self.options['logfile'] = str(value)
-      self.local_logfile_path = str(value)
-    elif key == 'binarypath':
-      self.options['binarypath'] = str(value)
-    elif key != 'onlyCommand':
-      raise BrowserStackLocalError('Attempted to pass invalid option to binary')
 
   def stop(self):
     try:
