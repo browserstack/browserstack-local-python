423d6a09f43e82be59f88267d952112f3931f4d8
Vibhaj Rajan <vibhaj8@gmail.com>
2016-05-01 16:08:32 +0530

    removed comments

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
 setup(
   name = 'browserstack-local',
-  packages = ['browserstack-local'], # this must be the same as the name above
+  packages = ['browserstack-local'],
   version = '0.1.0',
   description = 'Python bindings from Browserstack Local',
   author = 'BrowserStack',
   author_email = 'support@browserstack.com',
-  url = 'https://github.com/browserstack/browserstack-local-python', # use the URL to the github repo
-  download_url = 'https://github.com/browserstack/browserstack-local-python/tarball/0.1.0', # I'll explain this in a second
-  keywords = ['BrowserStack', 'Local', 'selenium', 'testing'], # arbitrary keywords
+  url = 'https://github.com/browserstack/browserstack-local-python',
+  download_url = 'https://github.com/browserstack/browserstack-local-python/tarball/0.1.0',
+  keywords = ['BrowserStack', 'Local', 'selenium', 'testing'],
   classifiers = [],
 )
 
