2983bb4613341f0a5b300313bed551a5cd01cd3c
Vibhaj Rajan <vibhaj8@gmail.com>
2016-05-09 12:25:45 +0530

    bumped up version

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'browserstack-local',
   packages = ['browserstack'],
-  version = '0.3.0',
+  version = '0.3.1',
   description = 'Python bindings for Browserstack Local',
   author = 'BrowserStack',
   author_email = 'support@browserstack.com',
   url = 'https://github.com/browserstack/browserstack-local-python',
-  download_url = 'https://github.com/browserstack/browserstack-local-python/archive/v0.1.0.tar.gz',
+  download_url = 'https://github.com/browserstack/browserstack-local-python/archive/master.zip',
   keywords = ['BrowserStack', 'Local', 'selenium', 'testing'],
   classifiers = [],
 )
