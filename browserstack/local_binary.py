ab6e091f136f19adc0df3dd897bd6011280018ab
Nels Chantarotwong <43188172+nchantarotwong@users.noreply.github.com>
2023-03-23 10:59:13 -0700

    switch back to spaces

--- browserstack/local_binary.py
+++ browserstack/local_binary.py
@@ -10,125 +10,125 @@ import urllib3
 from browserstack.bserrors import BrowserStackLocalError
 
 class LocalBinary:
-	"""BrowserStack LocalBinary class to download to local, also supporting proxies."""
-	def __init__(self, proxy=None):
-		is_64bits = sys.maxsize > 2 ** 32
-		self.is_windows = False
-		self.proxy = proxy
-		osname = platform.system()
-		if osname == 'Darwin':
-			self.http_path = "https://bstack-local-prod.s3.amazonaws.com/BrowserStackLocal-darwin-x64"
-		elif osname == 'Linux':
-			if self.is_alpine():
-				self.http_path = "https://bstack-local-prod.s3.amazonaws.com/BrowserStackLocal-alpine"
-			else:
-				if is_64bits:
-					self.http_path = "https://bstack-local-prod.s3.amazonaws.com/BrowserStackLocal-linux-x64"
-				else:
-					self.http_path = "https://bstack-local-prod.s3.amazonaws.com/BrowserStackLocal-linux-ia32"
-		else:
-			self.is_windows = True
-			self.http_path = "https://bstack-local-prod.s3.amazonaws.com/BrowserStackLocal.exe"
+    """BrowserStack LocalBinary class to download to local, also supporting proxies."""
+    def __init__(self, proxy=None):
+        is_64bits = sys.maxsize > 2 ** 32
+        self.is_windows = False
+        self.proxy = proxy
+        osname = platform.system()
+        if osname == 'Darwin':
+            self.http_path = "https://bstack-local-prod.s3.amazonaws.com/BrowserStackLocal-darwin-x64"
+        elif osname == 'Linux':
+            if self.is_alpine():
+                self.http_path = "https://bstack-local-prod.s3.amazonaws.com/BrowserStackLocal-alpine"
+            else:
+                if is_64bits:
+                    self.http_path = "https://bstack-local-prod.s3.amazonaws.com/BrowserStackLocal-linux-x64"
+                else:
+                    self.http_path = "https://bstack-local-prod.s3.amazonaws.com/BrowserStackLocal-linux-ia32"
+        else:
+            self.is_windows = True
+            self.http_path = "https://bstack-local-prod.s3.amazonaws.com/BrowserStackLocal.exe"
 
-		self.ordered_paths = [
-			os.path.join(os.path.expanduser('~'), '.browserstack'),
-			os.getcwd(),
-			tempfile.gettempdir()
-		]
-		self.path_index = 0
-
-
-	def __available_dir(self):
-		while self.path_index < len(self.ordered_paths):
-			if self.__make_path(self.ordered_paths[self.path_index]):
-				final_path = self.ordered_paths[self.path_index]
-				self.path_index += 1
-				return final_path
-			else:
-				self.path_index += 1
-		raise BrowserStackLocalError('Error trying to download BrowserStack Local binary')
+        self.ordered_paths = [
+            os.path.join(os.path.expanduser('~'), '.browserstack'),
+            os.getcwd(),
+            tempfile.gettempdir()
+        ]
+        self.path_index = 0
 
 
-	def is_alpine(self):
-		grepOutput = subprocess.run("gfrep -w NAME /etc/os-release", capture_output=True, shell=True)
-		if grepOutput.stdout.decode('utf-8').find('Alpine') > -1:
-			return True
-		return False
+    def __available_dir(self):
+        while self.path_index < len(self.ordered_paths):
+            if self.__make_path(self.ordered_paths[self.path_index]):
+                final_path = self.ordered_paths[self.path_index]
+                self.path_index += 1
+                return final_path
+            else:
+                self.path_index += 1
+        raise BrowserStackLocalError('Error trying to download BrowserStack Local binary')
 
 
-	def __make_path(self, dest_path):
-		try:
-			if not os.path.exists(dest_path):
-				os.makedirs(dest_path)
-			return True
-		except:
-			return False
+    def is_alpine(self):
+        grepOutput = subprocess.run("gfrep -w NAME /etc/os-release", capture_output=True, shell=True)
+        if grepOutput.stdout.decode('utf-8').find('Alpine') > -1:
+            return True
+        return False
 
 
-	def download(self, chunk_size=8192, progress_hook=None):
-		print(f'Downloading BrowserStack Local binary, proxy ({self.proxy}).', flush=True)
-		if self.proxy:
-			proxy = urllib3.ProxyManager(self.proxy)
-			response = proxy.request('GET', self.http_path, preload_content=False)
-		else:
-			http = urllib3.PoolManager()
-			response = http.request('GET', self.http_path, preload_content=False)
-
-		total_size = int(response.headers['Content-Length'].strip())
-		bytes_so_far = 0
-
-		dest_parent_dir = self.__available_dir()
-		dest_binary_name = 'BrowserStackLocal'
-		if self.is_windows:
-			dest_binary_name += '.exe'
-
-		with open(os.path.join(dest_parent_dir, dest_binary_name), 'wb') as local_file:
-			for chunk in response.stream(chunk_size):
-				bytes_so_far += len(chunk)
-				if not chunk:
-					break
-
-				if progress_hook:
-					progress_hook(bytes_so_far, chunk_size, total_size)
-
-				try:
-					local_file.write(chunk)
-				except:
-					return self.download(chunk_size, progress_hook)
-
-		final_path = os.path.join(dest_parent_dir, dest_binary_name)
-		st = os.stat(final_path)
-		os.chmod(final_path, st.st_mode | stat.S_IXUSR)
-		return final_path
+    def __make_path(self, dest_path):
+        try:
+            if not os.path.exists(dest_path):
+                os.makedirs(dest_path)
+            return True
+        except:
+            return False
 
 
-	def __verify_binary(self, path):
-		try:
-			binary_response = subprocess.check_output([path, "--version"]).decode("utf-8")
-			pattern = re.compile("BrowserStack Local version \d+\.\d+")
-			return bool(pattern.match(binary_response))
-		except:
-			return False
+    def download(self, chunk_size=8192, progress_hook=None):
+        print(f'Downloading BrowserStack Local binary, proxy ({self.proxy}).', flush=True)
+        if self.proxy:
+            proxy = urllib3.ProxyManager(self.proxy)
+            response = proxy.request('GET', self.http_path, preload_content=False)
+        else:
+            http = urllib3.PoolManager()
+            response = http.request('GET', self.http_path, preload_content=False)
+
+        total_size = int(response.headers['Content-Length'].strip())
+        bytes_so_far = 0
+
+        dest_parent_dir = self.__available_dir()
+        dest_binary_name = 'BrowserStackLocal'
+        if self.is_windows:
+            dest_binary_name += '.exe'
+
+        with open(os.path.join(dest_parent_dir, dest_binary_name), 'wb') as local_file:
+            for chunk in response.stream(chunk_size):
+                bytes_so_far += len(chunk)
+                if not chunk:
+                    break
+
+                if progress_hook:
+                    progress_hook(bytes_so_far, chunk_size, total_size)
+
+                try:
+                    local_file.write(chunk)
+                except:
+                    return self.download(chunk_size, progress_hook)
+
+        final_path = os.path.join(dest_parent_dir, dest_binary_name)
+        st = os.stat(final_path)
+        os.chmod(final_path, st.st_mode | stat.S_IXUSR)
+        return final_path
 
 
-	def get_binary(self):
-		dest_parent_dir = os.path.join(os.path.expanduser('~'), '.browserstack')
-		if not os.path.exists(dest_parent_dir):
-			os.makedirs(dest_parent_dir)
-		bsfiles = [f for f in os.listdir(dest_parent_dir) if f.startswith('BrowserStackLocal')]
+    def __verify_binary(self, path):
+        try:
+            binary_response = subprocess.check_output([path, "--version"]).decode("utf-8")
+            pattern = re.compile("BrowserStack Local version \d+\.\d+")
+            return bool(pattern.match(binary_response))
+        except:
+            return False
 
-		if len(bsfiles) == 0:
-			binary_path = self.download()
-		else:
-			binary_path = os.path.join(dest_parent_dir, bsfiles[0])
 
-		valid_binary = self.__verify_binary(binary_path)
-		if valid_binary:
-			return binary_path
-		else:
-			binary_path = self.download()
-			valid_binary = self.__verify_binary(binary_path)
-			if valid_binary:
-				return binary_path
-			else:
-				raise BrowserStackLocalError('BrowserStack Local binary is corrupt')
+    def get_binary(self):
+        dest_parent_dir = os.path.join(os.path.expanduser('~'), '.browserstack')
+        if not os.path.exists(dest_parent_dir):
+            os.makedirs(dest_parent_dir)
+        bsfiles = [f for f in os.listdir(dest_parent_dir) if f.startswith('BrowserStackLocal')]
+
+        if len(bsfiles) == 0:
+            binary_path = self.download()
+        else:
+            binary_path = os.path.join(dest_parent_dir, bsfiles[0])
+
+        valid_binary = self.__verify_binary(binary_path)
+        if valid_binary:
+            return binary_path
+        else:
+            binary_path = self.download()
+            valid_binary = self.__verify_binary(binary_path)
+            if valid_binary:
+                return binary_path
+            else:
+                raise BrowserStackLocalError('BrowserStack Local binary is corrupt')
