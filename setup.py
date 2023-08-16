e4b0e7a2c19d12cce07162f745814f65f898a53d
Ryan Rehman <ryanrehman99@gmail.com>
2023-08-16 16:59:05 +0530

    Bump version to 1.2.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 setup(
   name = 'browserstack-local',
   packages = ['browserstack'],
-  version = '1.2.5',
+  version = '1.2.6',
   description = 'Python bindings for Browserstack Local',
   author = 'BrowserStack',
   author_email = 'support@browserstack.com',
